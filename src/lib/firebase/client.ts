06d028de8f0f9575e18a07bfee3c0e3fccab5b60
ICP Fertilizer <icpfertilizer@gmail.com>
2025-09-04 05:06:41 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -1,10 +1,24 @@
 import { getApp, getApps, initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, connectAuthEmulator } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 import { firebaseConfig } from './config';
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getDatabase(app);
+
+if (process.env.NODE_ENV === 'development') {
+    // Point to the RTDB emulator running on localhost.
+    // connectDatabaseEmulator(db, "localhost", 9000);
+    
+    // Point to the Auth emulator running on localhost.
+    try {
+        connectAuthEmulator(auth, "http://localhost:9099", { disableWarnings: true });
+        console.log("Firebase Auth emulator connected");
+    } catch(e) {
+        console.warn("Could not connect to Firebase Auth emulator. If you are not running it, this is expected.");
+    }
+}
+
 
 export { app, auth, db };
